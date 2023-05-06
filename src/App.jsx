0780f7f9cf7a468a1ee78718d2f4e577646c5870
Muhammad Enamul Haque <muhammad.enamul.haque79@gmail.com>
2023-05-06 10:19:39 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='container'>
+      <div className="left-div"><img src="https://i.ibb.co/yFWWmks/laptop.png" alt="" /> </div>
+      <div className="right-div"><h1>Two div's beside are responsive containing with 50% space</h1></div>
+    </div>
+  )
+
+  }
+export default App;
+      
